bfd468e27aff44c7af594c61a1f85f0a2e26134a
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-03-14 15:26:35 +0530

    Create AllayMcPlugin.java

---
+++ AllayMcPlugin.java
@@ -0,0 +1,94 @@
+package com.allaymc.exile;
+
+import com.allaymc.exile.command.ExileCommand;
+import com.allaymc.exile.data.PlayerDataManager;
+import com.allaymc.exile.listener.PlayerJoinQuitListener;
+import com.allaymc.exile.listener.PlayerRespawnListener;
+import com.allaymc.exile.service.ExileService;
+import com.allaymc.exile.util.MessageUtil;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AllayMcPlugin extends JavaPlugin {
+
+    private static AllayMcPlugin instance;
+
+    private PlayerDataManager playerDataManager;
+    private ExileService exileService;
+    private MessageUtil messageUtil;
+
+    public static AllayMcPlugin getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        saveDefaultConfig();
+        saveResourceIfNotExists("messages.yml");
+
+        this.messageUtil = new MessageUtil(this);
+        this.playerDataManager = new PlayerDataManager(this);
+        this.exileService = new ExileService(this, playerDataManager, messageUtil);
+
+        registerCommands();
+        registerListeners();
+
+        exileService.startTimerTask();
+        getLogger().info("AllayMc enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        exileService.saveAllOnlineExileStates();
+        playerDataManager.saveAll();
+        getLogger().info("AllayMc disabled.");
+    }
+
+    private void registerCommands() {
+        ExileCommand exileCommand = new ExileCommand(this, exileService, playerDataManager, messageUtil);
+
+        bind("exile", exileCommand);
+        bind("exileadd", exileCommand);
+        bind("exilefree", exileCommand);
+        bind("exileextend", exileCommand);
+        bind("exileremove", exileCommand);
+        bind("exilecount", exileCommand);
+    }
+
+    private void bind(String name, ExileCommand executor) {
+        PluginCommand cmd = getCommand(name);
+        if (cmd != null) {
+            cmd.setExecutor(executor);
+            cmd.setTabCompleter(executor);
+        }
+    }
+
+    private void registerListeners() {
+        getServer().getPluginManager().registerEvents(
+                new PlayerJoinQuitListener(this, exileService, playerDataManager), this
+        );
+        getServer().getPluginManager().registerEvents(
+                new PlayerRespawnListener(this, exileService, playerDataManager), this
+        );
+    }
+
+    private void saveResourceIfNotExists(String path) {
+        if (!new java.io.File(getDataFolder(), path).exists()) {
+            saveResource(path, false);
+        }
+    }
+
+    public PlayerDataManager getPlayerDataManager() {
+        return playerDataManager;
+    }
+
+    public ExileService getExileService() {
+        return exileService;
+    }
+
+    public MessageUtil getMessageUtil() {
+        return messageUtil;
+    }
+}
